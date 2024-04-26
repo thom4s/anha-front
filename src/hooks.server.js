87839fed8bfec09ee9cd6001a78c5a19ac4097c3
Thomas <hello@thomasflorentin.net>
2024-04-26 09:24:58 +0200

    error: adding tpl

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,17 @@
+function redirect(location, body) {
+    return new Response(body, {
+        status: 303,
+        headers: { location }
+    });
+}
+
 export const handle = async ({ event, resolve }) => {
 
 	console.log(`📣 NEW REQUEST IS BEING MADE FROM ${event.url.pathname}`)
 	console.log('hooks.server.ts')
+
+	if ( event.url.pathname == '')
+		return redirect('/fr', '');
 
 	console.log(`🔥 THE PAGE IS READY, I'M SENDING THE RESPONSE`)
 
---
+++ src/routes/+error.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<main>
+
+    <div class="notFound-container wrapper flex-vertical gap-s">
+        
+        <h1 class="status-code">{$page.status}</h1>
+        <h2 class="h2">{$page.error.message}</h2>
+
+        <div class="actions flex-center gap-l">
+            <a class="btn btn_secondary" href="" data-sveltekit-reload>Try to reload first</a>
+            <a class="btn btn_secondary" href={ ($page.error.link) ? $page.error.link : '/'} data-sveltekit-prefetch>No ? Ok, go back</a>
+        </div>
+    </div>
+
+</main>
+
+<style lang="scss">
+
+    .notFound-container {
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .status-code {
+        font-size: 125px;
+        color: $primary-color;
+        font-weight: $font-b;
+    }
+    .h2 {
+        font-size: 24px;
+    }
+    .actions {
+        margin-top: 80px;
+    }
+
+</style>
