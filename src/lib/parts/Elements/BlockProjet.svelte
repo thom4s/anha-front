7b4f0db3eeb0e280b99102b5208536b9845104d8
Thomas <hello@thomasflorentin.net>
2024-05-23 14:20:46 +0200

    Update BlockProjet.svelte

--- src/lib/parts/Elements/BlockProjet.svelte
+++ src/lib/parts/Elements/BlockProjet.svelte
@@ -15,15 +15,13 @@
             {/each}
         {/if}
 
-        <a href="#" class="h4">
-            <h4 class="h4 no-margin">
+        <h4 class="h4 no-margin">
                 {projet.title}
                 <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 8.62305H13.7559" stroke="black" stroke-linecap="square" stroke-linejoin="round"/>
                     <path d="M9.5 4.12305L14 8.62305L9.5 13.123" stroke="black" stroke-linecap="square"/>
                 </svg>
-            </h4>
-        </a>
+        </h4>
 
         <div class="bloc_metadata fl-justify">
             {#if projet.informationsProjet.meta_date }
@@ -76,5 +74,9 @@
         img {
             aspect-ratio: 5/4;
         }
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
     }
 </style>
