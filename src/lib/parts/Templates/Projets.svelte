5bfeeba006f2872aa85fac0e44807baec2b2d4fc
Thomas <hello@thomasflorentin.net>
2025-06-27 13:11:02 +0200

    Update Projets.svelte

--- src/lib/parts/Templates/Projets.svelte
+++ src/lib/parts/Templates/Projets.svelte
@@ -75,7 +75,7 @@
                 return ( filtersSavoirfaire.includes(project.informationsProjet.tax_savoirfaire?.nodes[0]?.name) || filtersSavoirfaire.includes(project.informationsProjet.tax_savoirfaire?.nodes[1]?.name) ) && ( filtersSecteur.includes(project.informationsProjet.tax_secteur?.nodes[0]?.name) || filtersSecteur.includes(project.informationsProjet.tax_secteur?.nodes[1]?.name) )
             }
             else if(filtersSavoirfaire.length !== 0 && filtersSecteur.length === 0 ) {
-                return filtersSavoirfaire.includes(project.informationsProjet.tax_savoirfaire?.nodes[0].name) || filtersSavoirfaire.includes(project.informationsProjet.tax_savoirfaire?.nodes[1]?.name)
+                return filtersSavoirfaire.includes(project.informationsProjet.tax_savoirfaire?.nodes[0].name)
             }
             else if(filtersSecteur.length !== 0 && filtersSavoirfaire.length === 0 ) {
                 return filtersSecteur.includes(project.informationsProjet.tax_secteur?.nodes[0].name) || filtersSecteur.includes(project.informationsProjet.tax_secteur?.nodes[1]?.name)
