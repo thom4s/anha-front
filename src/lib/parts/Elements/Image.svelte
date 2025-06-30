bde69cc792bfe0cd2527cd8b205b893db6b6847d
Thomas <hello@thomasflorentin.net>
2025-06-30 15:45:06 +0200

    Update Image.svelte

--- src/lib/parts/Elements/Image.svelte
+++ src/lib/parts/Elements/Image.svelte
@@ -40,7 +40,7 @@
 <div class="media_container" data-ratio="{ratio}">
     <img 
         src="{node?.sourceUrl}" 
-        alt="{ node?.caption.replace(/<\/?[^>]+(>|$)/g, "") }"
+        alt="{ node?.caption }"
         srcset="{node?.srcSet}"
         sizes="{node?.sizes}" 
         bind:this={imgElement}
