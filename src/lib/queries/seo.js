82c536b6f672af93f866f5040fe4e0b574e54861
Tom Bost <74453277+Jok-Rr@users.noreply.github.com>
2026-09-15 17:41:59 +0200

    Revert "fix: remove debug log"
    This reverts commit 6470936c96105034eb9a82c07882318392854299.

--- src/lib/queries/seo.js
+++ src/lib/queries/seo.js
@@ -4,6 +4,24 @@ import { PUBLIC_API_URL } from "$env/static/public";
 
 export async function getSeoSchema( ) {
 
+    try {
+        return await getSeoSchemaFetch();
+    } catch (err) {
+        console.log('DEBUG getSeoSchema fetch error', JSON.stringify({
+            message: err?.message,
+            code: err?.cause?.code,
+            errno: err?.cause?.errno,
+            syscall: err?.cause?.syscall,
+            address: err?.cause?.address,
+            port: err?.cause?.port,
+            name: err?.cause?.name
+        }));
+        throw err;
+    }
+}
+
+async function getSeoSchemaFetch( ) {
+
     const seoConfig = await fetch(PUBLIC_API_URL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -63,7 +81,19 @@ export async function getSeoSchema( ) {
             `
             }),
         })
-        .then(res => res.json())
+        .then(async res => {
+            const text = await res.text();
+            try {
+                return JSON.parse(text);
+            } catch (e) {
+                console.log('DEBUG getSeoSchema non-json response', JSON.stringify({
+                    status: res.status,
+                    contentType: res.headers.get('content-type'),
+                    body: text.slice(0, 500)
+                }));
+                throw e;
+            }
+        })
         .then(res => {
             return res.data
         });
